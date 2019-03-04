a01d51eada3abbb8b9b7558e75637542042302c4
Jan Krupa <jankrupa93@gmail.com>
2019-03-04 11:00:21 +0100

    Dotenv module for loading enviroment variables added

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+// curl 127.0.0.1:3000
+app.get('/', function (req, res) {
+   res.send(process.env.DB_HOST + ' ' + process.env.DB_USER);
+})
+
 app.listen(port);
 
 console.log('To-Do list RESTful API server started on: ' + port);
